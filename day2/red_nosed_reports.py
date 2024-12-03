c08d4dc253d3c575e2b1358d4b4cd26d74f1e829
Laura Reese <laurareese03@gmail.com>
2024-12-02 21:00:50 -0600

    day2: use functions to prettify code

--- day2/red_nosed_reports.py
+++ day2/red_nosed_reports.py
@@ -10,17 +10,18 @@ def check_direction_change(change_levels):
 def check_large_gaps(change_levels):
   return len({level for level in change_levels if level > 3 or level < -3}) > 0
 
+def check_safety(test_change_levels):
+  return not (check_no_change(set(test_change_levels)) or check_direction_change(set(test_change_levels)) or check_large_gaps(set(test_change_levels)))
+
+def get_change_levels(test_report):
+  return [test_report[i]-test_report[i+1] for i in range(len(test_report)-1)]
+
 count_a = 0
 for line in lines:
   report = list(map(int,line.strip().split(' ')))
-  change_levels = set([report[i]-report[i+1] for i in range(len(report)-1)])
-  if 0 in change_levels:
-    continue
-  if len(set([level > 0 for level in change_levels])) > 1:
-    continue
-  if len({level for level in change_levels if level > 3 or level < -3}) > 0:
-    continue
-  count_a +=1
+  change_levels = get_change_levels(report)
+  if check_safety(change_levels):
+    count_a +=1
 print(count_a)
 
 # I'm sure there's a way to shorten this so we don't iter over all change levels for failing levels
@@ -28,15 +29,15 @@ print(count_a)
 count_b = 0
 for line in lines:
   report = list(map(int,line.strip().split(' ')))
-  change_levels = [report[i]-report[i+1] for i in range(len(report)-1)]
-  if  not (check_no_change(set(change_levels)) or check_direction_change(set(change_levels)) or check_large_gaps(set(change_levels))):
+  change_levels = get_change_levels(report)
+  if check_safety(change_levels):
     count_b += 1
-  else:
-    for i in range(len(report)):
-      test_report = report.copy()
-      del test_report[i]
-      test_change_levels = [test_report[i]-test_report[i+1] for i in range(len(test_report)-1)]
-      if not (check_no_change(set(test_change_levels)) or check_direction_change(set(test_change_levels)) or check_large_gaps(set(test_change_levels))):
-        count_b += 1
-        break
+    continue
+  for i in range(len(report)):
+    test_report = report.copy()
+    del test_report[i]
+    test_change_levels = get_change_levels(test_report)
+    if check_safety(test_change_levels):
+      count_b += 1
+      break
 print(count_b)
