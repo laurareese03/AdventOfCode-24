66c1f983ab5fde546126c00dbdf4bba93edf3a58
Laura Reese <laurareese03@gmail.com>
2024-12-05 20:46:35 -0600

    day5: part b, not as bad as expected

--- day5/print_queue.py
+++ day5/print_queue.py
@@ -3,7 +3,7 @@ page_orders = lines[0].split('\n')
 pages = lines[1].split('\n')
 
 # orderings dict tracts all pages that should be smaller than the current page
-orderings = {}
+orderings = {'13': []}
 for order in page_orders:
   comp = order.split('|')
   try:
@@ -11,6 +11,7 @@ for order in page_orders:
   except:
     orderings[comp[0]] = [comp[1]]
 
+part_b_pages = []
 # brute forcing because the orders in a cycle! wtf!
 page_total = 0
 complete = True
@@ -20,6 +21,29 @@ for page in pages:
   for i in range(len(page)):
     if len(set(page[:i]).intersection(set(orderings[page[i]]))) != 0:
       complete = False
+      part_b_pages.append(page)
       break
   if complete: page_total += int(page[(len(page)-1)//2])
+# part a
 print(page_total)
+
+sorted_page_total = 0
+for page in part_b_pages:
+  page_orderings = {}
+  for p in page:
+    page_orderings[p] = set(page).intersection(set(orderings[p]))
+    
+  # lifted almost exactly from my initial attempt at part a lol
+  # sometimes being goofy pays off!
+  exact_order = []
+  for i in range(len(page_orderings)):
+    for order in page_orderings:
+      if len(page_orderings[order])  == len(exact_order) + 1:
+        exact_order.append(order)
+  for order in page_orderings:
+    if len(page_orderings[order]) == 1 and page_orderings[order] not in exact_order:
+        exact_order.insert(0, str(list(page_orderings[order])[0]))
+  sorted_page_total += int(exact_order[(len(exact_order)-1)//2])
+
+# part b
+print(sorted_page_total)
