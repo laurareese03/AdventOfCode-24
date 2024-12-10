15c46512a21a26be77e8f1366b5494b6dcd9e6c1
Laura Reese <laurareese03@gmail.com>
2024-12-09 22:26:40 -0600

    day9 partb: nested lists was the wrong choice

--- day9/disk_fragmenter.py
+++ day9/disk_fragmenter.py
@@ -28,3 +28,38 @@ for i in range(len(blocks)):
   checksum += i * blocks[i]
 
 print(checksum)
+
+blocks = []
+# reset blocks
+for i in range(len(components)):
+  if i % 2 == 0:
+    blocks.append([i//2]*int(components[i]))
+  else:
+    blocks.append(['.']*int(components[i]))
+
+# using a while loop here bc inserting files and empty space messes with indexing
+i = len(blocks)-1
+while i > 0:
+  for j in range(i):
+    if blocks[i] and blocks[j].count('.') >= len(blocks[i]) and blocks[i].count('.') == 0:
+      new_space = ['.']*(len(blocks[j])-len(blocks[i]))
+      # move files to new location and replace former location with empty space
+      blocks[j] = blocks[i]
+      blocks[i] = ['.']*len(blocks[i])
+      # if there's still space left at the current location, add it to the block info, and readjust indexing
+      if new_space != []:
+        blocks.insert(j+1, new_space)
+        i +=1
+      break
+  i -= 1
+
+checksum = 0
+list_1d = []
+# convert nested lists into 1d list
+for i in range(len(blocks)):
+  list_1d += blocks[i]
+
+for i in range(len(list_1d)):
+  if list_1d[i] != '.':
+    checksum += int(list_1d[i]) * i
+print(checksum)
