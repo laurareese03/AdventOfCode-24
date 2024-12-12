17e95d84f0f24c7cfe62470c4f17973430569a91
Laura Reese <laurareese03@gmail.com>
2024-12-11 20:22:16 -0600

    day11: SO much cleaner

--- day11/plutonian_pebbles.py
+++ day11/plutonian_pebbles.py
@@ -1,19 +1,23 @@
+# not actually sure if its quicker to limit known stones to just the core 54 values
+# but its certainly easier to understand and debug so. it stays.
+# (its probably quicker)
 def build_known_stones():
   stones = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  blink = 0
-  while blink < 13:
-    next_stones = []
-    for i in range(len(stones)):
-      if stones[i] == 0:
-        next_stones.append(1)
-      elif len(str(stones[i])) % 2 == 0:
-        next_stones.append(int(str(stones[i])[:len(str(stones[i]))//2]))
-        next_stones.append(int(str(stones[i])[len(str(stones[i]))//2:]))
-      else:
-        next_stones.append(stones[i] * 2024)
-    stones = next_stones
-    blink += 1
-  return set(stones)
+  common_stones = set()
+  for stone in stones:
+    if len(str(stone)) % 2 == 0:
+      left, right = int(str(stone)[:len(str(stone))//2]), int(str(stone)[len(str(stone))//2:])
+      if left not in common_stones:
+        stones.append(left)
+        common_stones.add(left)
+      if right not in common_stones:
+        stones.append(right)
+        common_stones.add(right)
+    else:
+      if stone * 2024 not in common_stones:
+        stones.append(stone * 2024)
+        common_stones.add(stone * 2024)
+  return dict.fromkeys(common_stones, 0)
 
 def handle_known_stones(known_stones):
   holder = dict.fromkeys(known_stones.keys(), 0)
@@ -30,36 +34,40 @@ def handle_known_stones(known_stones):
 def handle_unknown_stones(unknown_stones, known_stones):
   holder_unknown = []
   for stone in unknown_stones:
-      if len(str(stone)) % 2 == 0:
-        left = int(str(stone)[:len(str(stone))//2])
-        right = int(str(stone)[len(str(stone))//2:])
-        if left in known_stones.keys():
-          known_stones[left] += 1
-        else:
-          holder_unknown.append(left)
-        if right in known_stones.keys():
-          known_stones[right] += 1
-        else: 
-          holder_unknown.append(right)
+    # we don't have to worry about the 0 bc it should already be in known_stones
+    if len(str(stone)) % 2 == 0:
+      s = str(stone)
+      left, right = int(s[:len(s)//2]), int(str(stone)[len(s)//2:])
+      if left in known_stones.keys():
+        known_stones[left] += 1
       else:
-        holder_unknown.append(stone*2024)
+        holder_unknown.append(left)
+      if right in known_stones.keys():
+        known_stones[right] += 1
+      else: 
+        holder_unknown.append(right)
+    else:
+      # we don't have to check presense in known_stones bc they would already be moved
+      holder_unknown.append(stone*2024)
   return holder_unknown, known_stones
 
 def get_count(known, unknown):
   return sum(known.values()) + len(unknown)
 
-unique_stones = build_known_stones()
-known_stones = dict.fromkeys(unique_stones, 0)
-known_stones[0] = 1
-known_stones[1] = 1
-unknown_stones = [int(stone) for stone in open('stones.txt').read().split(' ') if int(stone) > 1]
-for i in range(75):
+# build our dictionary of common rocks and list of unusual ones to get started
+known_stones = build_known_stones()
+unknown_stones = [int(stone) for stone in open('stones.txt').read().split(' ')]
+for stone in unknown_stones:
+  if stone in known_stones.keys():
+    known_stones[stone] += 1
+    unknown_stones.remove(stone)
+
+# don't blink, don't even blink, blink and you're dead
+for blink in range(75):
   known_stones = handle_known_stones(known_stones)
   unknown_stones, known_stones = handle_unknown_stones(unknown_stones, known_stones)
-
   # part a
-  if i == 24:
+  if blink == 24:
     print(get_count(known_stones, unknown_stones))
-
 # part b
 print(get_count(known_stones, unknown_stones))
