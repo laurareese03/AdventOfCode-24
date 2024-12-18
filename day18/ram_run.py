7e3429a966f145efcae944c5e6d7c3dd9bda67ae
Laura Reese <laurareese03@gmail.com>
2024-12-18 00:04:09 -0600

    day18 partb: the brute force gods are smiling at us this AoC season

--- day18/ram_run.py
+++ day18/ram_run.py
@@ -1,10 +1,11 @@
 import numpy as np, queue as q
 map = np.full((71,71), '.')
 
-bites = open('falling_bytes.txt').readlines()
+bites = open('falling_bytes.txt').read().split('\n')
 for i in range(1024):
   bite = bites[i].split(',')
   map[int(bite[1]), int(bite[0])] = '#'
+map_holder = map.copy()
 
 start_index = (0,0)
 end_index = (70,70)
@@ -19,12 +20,25 @@ def get_next_points(index):
         map[mi[i]] = 'X'
   return points
 
-frontier = q.Queue()
-frontier.put((start_index, 0))
-while not frontier.empty():
-  current_index = frontier.get()
-  if current_index[0] == end_index:
-    print(current_index[1])
+def get_shortest_path():
+  frontier = q.Queue()
+  frontier.put((start_index, 0))
+  while not frontier.empty():
+    current_index = frontier.get()
+    if current_index[0] == end_index:
+      return current_index[1]
+    for point in get_next_points(current_index[0]):
+      frontier.put((point, current_index[1] + 1))
+  return -1
+# part a
+print(get_shortest_path())
+
+for i in range(1024, len(bites)):
+  map = map_holder
+  bite = bites[i].split(',')
+  map[int(bite[1]), int(bite[0])] = '#'
+  map_holder = map.copy()
+  if get_shortest_path() == -1:
+    # part b
+    print(bites[i])
     break
-  for point in get_next_points(current_index[0]):
-    frontier.put((point, current_index[1] + 1))
