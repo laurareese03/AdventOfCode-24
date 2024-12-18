daef7f5ab3e1f6017189e6478596b063b8696323
Laura Reese <laurareese03@gmail.com>
2024-12-17 19:13:47 -0600

    day16 parts a+b: clean to 1 a* pass for both

--- day16/reindeer_maze.py
+++ day16/reindeer_maze.py
@@ -1,109 +1,54 @@
-import numpy as np, queue as q, sys
-np.set_printoptions(linewidth=1000)
-
+import numpy as np, queue as q
 maze = np.array([list(line) for line in open('maze.txt').read().split('\n')], np.dtype('U4'))
-maze_b = maze.copy()
 
+# get our start/end points and replace them with the empty space character
 start_index = np.nonzero(maze == 'S')
-start_index = (start_index[0][0].item(), start_index[1][0].item())
+start_index = (start_index[0][0].item(), start_index[1][0].item()) # if you remove this line, numpy gives a deprecation warning
+maze[start_index] = '.'
 end_index = np.nonzero(maze == 'E')
+end_index = (end_index[0][0].item(), end_index[1][0].item()) # i love/h8 numpy
 maze[end_index] = '.'
-end_index = (end_index[0][0].item(), end_index[1][0].item())
 
-def get_next_points(index):
+def get_next_points(index, steps):
   points = []
-  if maze[index[0]-1, index[1]] == '.': # up
-    points.append(((index[0]-1, index[1]), 0))
-    maze[index[0]-1, index[1]] = 'X'
-  if maze[index[0], index[1]+1] == '.': # right
-    points.append(((index[0], index[1]+1), 1))
-    maze[index[0], index[1]+1] = 'X'
-  if maze[index[0]+1, index[1]] == '.': # down
-    points.append(((index[0]+1, index[1]), 2))
-    maze[index[0]+1, index[1]] = 'X'
-  if maze[index[0], index[1]-1] == '.': # left
-    points.append(((index[0], index[1]-1), 3))
-    maze[index[0], index[1]-1] = 'X'
-  return points
-
-def get_next_points_b(index, steps):
-  points = []
-  steps = str(steps).zfill(2)
-  mi = [(index[0]-1, index[1]), (index[0], index[1]+1), (index[0]+1, index[1]), (index[0], index[1]-1)]
+  mi = [(index[0]-1, index[1]), (index[0], index[1]+1), (index[0]+1, index[1]), (index[0], index[1]-1)] # t shape directions
   for i in range(len(mi)):
-    if maze[mi[i]] != '#' and int(maze[mi[i]] >= steps) or maze[mi[i]] == '.':
+    # '.' means the index hasn't been visited yet, otherwise there should be a wall we don't turn into 
+    # or a number corresponding to the step it was visited on. if our current step is less than the next index,
+    # we still add it to the frontier bc A* may have searched further on a different index bc of the turn score heuristic
+    if maze[mi[i]] == '.' or maze[mi[i]] != '#' and int(maze[mi[i]]) >= steps:
       points.append((mi[i], i))
       maze[mi[i]] = steps
   return points
 
 min_score = 0
-# A* my beloved
+best_path_points = {}
+
 frontier = q.PriorityQueue()
-frontier.put((0, (start_index, 1))) # (score, (index, direction)) bc priority queue requires input as (score, data)
+frontier.put((0, (start_index, 1, 1, []))) # (score, (index, direction, steps, visited))
+
+# A* my beloved
 while not frontier.empty():
   current_index = frontier.get()
   if current_index[1][0] == end_index:
     min_score = current_index[0]
-    break
-  for point in get_next_points(current_index[1][0]):
-    step_score = 1 + current_index[0] if point[1] == current_index[1][1] else 1001 + current_index[0]
-    frontier.put((step_score, (point[0], point[1])))
-print(min_score)
-
-# dfs with depth limiting based on score
-maze = maze_b
-# frontier = q.LifoQueue()
-# frontier.put((0, (start_index, 1, []))) # (score, (index, direction, [visited points]))
-# best_path_points = {start_index}
-# while not frontier.empty():
-#   current_index = frontier.get()
-#   if current_index[1][0] == end_index:
-#     best_path_points.update(current_index[1][2])
-#   elif current_index[0] > min_score:
-#     continue
-#   for point in get_next_points_b(current_index[1][0]):
-#     if point[0] not in current_index[1][2]:
-#       visited = current_index[1][2].copy()
-#       visited.append(current_index[1][0])
-#       step_score = 1 + current_index[0] if point[1] == current_index[1][1] else 1001 + current_index[0]
-#       frontier.put((step_score, (point[0], point[1], visited)))
-#   print(visited, frontier.qsize(), current_index[0])
-# print(len(best_path_points)+1) # we accidentally don't count the end node as visited
-
-frontier = q.PriorityQueue()
-frontier.put((0, (start_index, 1, 1, []))) # (score, (index, direction)) bc priority queue requires input as (score, data)
-best_path_points = {}
-while not frontier.empty():
-  current_index = frontier.get()
-  if current_index[1][0] == end_index:
-    print(current_index[1][3])
     best_path_points = set(current_index[1][3])
     break
-  for point in get_next_points_b(current_index[1][0], current_index[1][2]):
+  for point in get_next_points(current_index[1][0], current_index[1][2]):
     visited = current_index[1][3].copy()
     visited.append(current_index[1][0])
     step_score = 1 + current_index[0] if point[1] == current_index[1][1] else 1001 + current_index[0]
     frontier.put((step_score, (point[0], point[1], current_index[1][2] + 1, visited)))
-  
-with open('output_file.txt', 'w') as writer:
-  for i in range(len(maze)):
-    np.putmask(maze[i], maze[i] == '#', ['##'])
-    np.putmask(maze[i], maze[i] == '.', ['..'])
-    string = np.array2string(maze[i]) + '\n'
-    writer.write(string)
 
-print(len(best_path_points), best_path_points)
+# the next n steps on the frontier should also be the end point, so we should check if their score is equal to the lowest
+# if it is, its a valid path, and we add its visited point to the set of best path points. 
+# Otherwise, min priority queue means anything after is too long, so we'll skip the rest
 while not frontier.empty():
   current_index = frontier.get()
   if current_index[0] == min_score:
-    print(current_index[1][3])
     best_path_points = best_path_points.union(set(current_index[1][3]))
   else:
-    print(current_index[1][0])
+    break
 
+print(min_score)
 print(len(best_path_points)+1)
-
-# 546 low
-# 665
-
-+ 21 + 3
