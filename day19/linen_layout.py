15fbbe8990917a9f43926bd64e1b06d2e0fb0ad6
Laura Reese <laurareese03@gmail.com>
2024-12-19 21:16:00 -0600

    day19 part b: its runs so quickly :')

--- day19/linen_layout.py
+++ day19/linen_layout.py
@@ -5,21 +5,29 @@ towels = sorted(towels.split(', '), key=len)
 towels.reverse()
 target_designs = target_designs.split('\n')
 
-fail_strings = set() # is this... memoization?
-
-def check_next_towel(word):
+def check_next_towel(word, valid_strings):
+  word_count = 0
   if word == '':
-    return True
+    return 1
   for towel in towels:
     match = re.match(towel, word)
-    if match and word not in fail_strings and check_next_towel(word[match.end():]):
-      return True
+    if match and word[match.end():] in valid_strings.keys():
+      word_count += valid_strings[word[match.end():]]
+    elif match and word not in fail_strings:
+      word_count += check_next_towel(word[match.end():], valid_strings)
     elif match:
       fail_strings.add(word[match.end():])
-  return False
+    if match:
+      valid_strings[word] = word_count
+  return word_count
 
-count = 0
+fail_strings = set() # is this... memoization?
+valid_lines = 0
+valid_counts = 0
 for design in target_designs:
-  if check_next_towel(design):
-    count += 1
-print(count)
+  valid = check_next_towel(design, {})
+  valid_counts += valid
+  valid_lines += int(valid != 0)
+
+print(valid_lines)
+print(valid_counts)
